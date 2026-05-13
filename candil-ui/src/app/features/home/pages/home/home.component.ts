fd3fd968d783bfb84a67dbeb2f659d1c93b7dc65
Eko09 <tuhack18@tutamail.com>
2026-05-12 22:32:52 -0500

    ajustes a login y css

---
+++ candil-ui/src/app/features/home/pages/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  template: `
+    <h1>HOME FUNCIONANDO</h1>
+  `
+})
+export class HomeComponent {}
